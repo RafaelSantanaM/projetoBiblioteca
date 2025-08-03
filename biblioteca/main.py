e8ec0aed8232c19fb05d7ce1faf210056e17eb4c
kamosara097 <rafalibs097@gmail.com>
2025-08-03 19:03:40 -0300

    modificações feitas em main.py adição e implementação para locação de livrvos.

--- biblioteca/main.py
+++ biblioteca/main.py
@@ -33,7 +33,7 @@ print('\nApós remover o livro:')
 biblioteca.listar_livros()
 
 # Teste de entrada de um novo usuario:
-print('\nCadastro de um novo usu´raio:')
+print('\nCadastro de um novo usuário:')
 nome = input('Digite o nome do usuário: ')
 email = input('Digite o email do usuário: ')
 id_usuario = input('Digite o ID do usuário: ')
@@ -44,3 +44,47 @@ usuario = Usuario(nome, email, id_usuario)
 # Exibe os detalhes do usuário:
 print('\nUsuário cadastrado com sucesso!')
 print(usuario)
+
+
+# Locação de umlivro para um usuario:
+print(f'\nLocação de livro para o usuario {usuario.nome}: ')
+livro_para_emprestar = input('Digite o código ISBN do livro a ser emprestado: ')
+
+#Busca o livro na biblioteca:
+buscar_livro_emprestado = biblioteca.buscar_livro(livro_para_emprestar)
+
+if buscar_livro_emprestado:
+    usuario.empresstar_livro(buscar_livro_emprestado)
+    print(f'\nLivro "{buscar_livro_emprestado.titulo}" emprestado por {usuario.nome}.')
+else:
+    print('Livro não encontrado na biblioteca.')
+    
+# Lista todos os livros emprestados pelo usuário:
+print(f'\nLivros atualmente emprestados pelo usuário {usuario.nome}:')
+usuario.listar_livros_emprestados()
+        
+# Devolução de um livro emprestado:
+print(f'\nDevolução de livro por {usuario.nome}: ')
+isbn_devolver = input('Digite o código ISBN do livro a ser devolvido: ')
+
+# Procura o livro na lista de livros emprestados pelo usuario:
+# Definindo um valor padrão (None) para o livro a ser devolvido
+livro_para_devolver = None
+# Iterando sobre os livros emprestados pelo usuário para encontrar o livro a ser devolvido
+for livro in usuario.livros_emprestados:
+    if livro.isbn == isbn_devolver:   # Se o código ISBN do livro coincidir com o fornecido
+        # Se encontrado, atribui o livro à variável livro_para_devolver
+        livro_para_devolver = livro
+        break
+    
+if livro_para_devolver:
+    # Se o livro for encontrado, chama o método devolver_livro do usuário
+    usuario.devolver_livro(livro_para_devolver)
+    print(f'\nLivro {livro_para_devolver.titulo} devolvido por {usuario.nome}.')
+else: 
+    print('\nEste livro não está emprestado por este usuário ou não foi encontrado.')
+    
+# Lista novamente todos os livros emprestados pelo usuário após a devolução:
+print(f'\nLivros atualmente emprestados pelo usuário {usuario.nome} após devolução: ')
+usuario.listar_livros_emprestados()
+    
